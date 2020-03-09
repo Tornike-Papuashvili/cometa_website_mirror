873b26e15fde8f4770c14861e51cfc4777d89d9d
Ruben Aguilera <amvararuben@gmail.com>
2020-03-09 14:48:02 +0100

    fix route on script

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 echo -e "\e[37mStartup cleanup...\e[0m"
 echo "" > output.log 2>&1
 echo -e "\e[37mCopying files to public folder...\e[0m"
-cp -a /code/dist/. /usr/share/nginx/html/ >> output.log 2>&1
+cp -a /cometa_website/code/. /usr/share/nginx/html/ >> output.log 2>&1
 echo -e "\e[32mOK\e[0m"
 echo -e "\e[32mSuccessful\e[0m"
 nginx -g 'daemon off;'
