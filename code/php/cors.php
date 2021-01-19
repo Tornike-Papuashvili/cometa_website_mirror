c5ffc615842dcbf9e4dfbd6633552f9899aca7e8
Alex Barba <leparadoxhdv@gmail.com>
2021-01-19 16:47:38 +0100

    Improve CORS and captcha for other AMVARA domains

--- code/php/cors.php
+++ code/php/cors.php
@@ -11,27 +11,33 @@
  *
  */
 function cors() {
-    
-    // Allow from any origin
+    $devMode = false; // Set to true to skip domain checking
     if (isset($_SERVER['HTTP_ORIGIN'])) {
-        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
-        // you want to allow, and if so:
-        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
-    }
+        $origin = $_SERVER['HTTP_ORIGIN'];
+        // Allow from AMVARA domains
+        $amvaraDomains = ['.amvara.de', '.amvara.consulting', '.amvara.rocks'];
+        $allowed = false;
+        foreach ($amvaraDomains as $domain) {
+            if (strpos($origin, $domain) !== FALSE) $allowed = true;
+        }
+        if ($allowed OR $devMode) {
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}"); // Allow requesting origin
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400');    // cache for 1 day
     
-    // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-        
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            // may also be using PUT, PATCH, HEAD etc
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
-        
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    
-        exit(0);
+            // Access-Control headers are received during OPTIONS requests
+            if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+                
+                if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+                    // may also be using PUT, PATCH, HEAD etc
+                    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+                
+                if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+                    header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+            
+                exit(0);
+            }
+        }
     }
 }
 ?>
