52eab99295296498e124b6257d4e66b80b287c04
Victor Lopez <vlopezgirones@gmail.com>
2021-04-18 17:12:20 +0200

    Deleted Old Files

--- combine.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-cat code/js/jquery.js code/js/bootstrap.min.js code/js/mousescroll.js code/js/wow.min.js code/js/scrolling-nav.js code/js/jquery-image-viewer-magnify/js/jquery.magnify.min.js code/js/main.js > code/js/combined.js
-
-
-cat code/css/bootstrap.min.css code/css/font-awesome.min.css code/css/animate.min.css code/js/jquery-image-viewer-magnify/css/jquery.magnify.min.css code/js/jquery-image-viewer-magnify/css/snack.css code/js/jquery-image-viewer-magnify/css/snack-helper.css code/js/jquery-image-viewer-magnify/css/docs.css > code/css/combined.css
-
